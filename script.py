e786c4e14edb006e7b4bd09fa8df0cbc7a6c2ee0
Bates Drones <78171408+batesdrones@users.noreply.github.com>
2022-03-05 13:07:45 -0500

    Added geolocation by IP address feature.

--- script.py
+++ script.py
@@ -1,26 +1,31 @@
 import csv
 import time
 import pandas as pd
-from geopy.geocoders import Nominatim
+import urllib.request
+import geocoder
 import geopy.distance
 import gmplot
 import folium
 import webbrowser
 
 ###get location of starting point
-###update getLoc address for new starting location
-loc = Nominatim(user_agent="GetLoc")
-getLoc = loc.geocode("148 Nelson St, Brooklyn, NY")
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 
-starting_point = (getLoc.latitude, getLoc.longitude)
+ip = geocoder.ip(f'{external_ip}')
+starting_point = ip.latlng
 
-###ask the user for length willing to travel. Create the circle radius
-###todo: raise exceptions if not an integer
+
+"""
+ask the user for length willing to travel. Create the circle radius
+"""
+#todo: raise exceptions if not an integer
 radius_question = input("How many miles are you willing to travel: ")
 radius = int(radius_question)
 
-###alternative method to create a new df column named Distance.
-###Then filter df to sample only rows with values below radius input
+"""
+alternative method to create a new df column named Distance- then
+filter df to sample only rows with values below radius input
+"""
 
 def filterDistance():
     global df
@@ -42,8 +47,10 @@ def filterDistance():
 
 filterDistance()
 
-###todo: convert distance to less decimals for display
-###removes object number from variables with item method
+#todo: convert distance to less decimals for display
+"""
+removes object number from variables with item method
+"""
 ending_point = ending_point.item()
 destination = destination.item()
 line = line.item()
@@ -51,7 +58,7 @@ line = line.item()
 print(f"Your next stop is: {destination} on the {line} line.")
 print(f"It is {distance} miles away.")
 
-###convert coordinates to float to use as coordinates for mapping
+#convert coordinates to float to use for mapping
 def convert_float(inp):
     splitted_data = inp.split(",")
     return float(splitted_data[-2]), float(splitted_data[-1])
@@ -60,10 +67,10 @@ lat, long = convert_float(ending_point)
 coords = [lat, long]
 
 def generate_map():
-    my_map = folium.Map(location = coords, zoom_start=13)
+    my_map = folium.Map(location = coords, zoom_start=10)
     folium.Marker(coords, popup = f"Your destination: {destination} station. Have fun!").add_to(my_map)
     my_map.save('map.html')
 
 generate_map()
 time.sleep(5)
-webbrowser.open('file:///Users/andybates/PycharmProjects/SubwayStationsApp/map.html')
+webbrowser.open('./map.html')
